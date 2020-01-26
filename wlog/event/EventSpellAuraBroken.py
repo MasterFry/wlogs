fe49e96afb097bc5630a8e46da528a8b9db171e3
Stefan Fragner <stefan.fragner95@gmail.com>
2020-01-26 18:05:18 +0100

    prefix params

---
+++ wlog/event/EventSpellAuraBroken.py
@@ -0,0 +1,27 @@
+from .Event import Event
+from .EventType import EventType
+from .EventBase import EventBase
+from .EventParser import EventParser
+
+class EventEventSpellAuraBroken(EventBase):
+    def __init__(self, time, parser: EventParser):
+        EventBase.__init__(self, time)
+
+        # Prefix Parameters are inbetween Base and Advanced Parameters
+        # e.g.: 11682,"Hellfire Effect",0x4
+        self.spellId = parser.getInt()
+        self.spellName = parser.getString()
+        self.spellSchool = parser.getInt(base=16)
+        
+
+    def getEventType(self) -> EventType:
+        return EventType.SPELL_AURA_BROKEN
+
+    def __str__(self):
+        return EventBase.__str__(self) +
+
+    def __eq__(self, other):
+        return EventBase.__eq__(other) and 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
